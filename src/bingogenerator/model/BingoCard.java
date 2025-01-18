ed914b90fd1eaaa19fdfed4c0755c6b67d000463
Itzac <itzacalbertin@gmail.com>
2025-01-18 01:12:40 -0300

    Gerando números em ordem e com limitações de acordo com a coluna.

--- src/bingogenerator/model/BingoCard.java
+++ src/bingogenerator/model/BingoCard.java
@@ -7,6 +7,14 @@ import java.util.List;
 public class BingoCard {
     private static final int LINHAS = 5;
     private static final int COLUNAS = 5;
+    private static final int [][] FAIXA = { //Limite de cada coluna
+            {1, 15}, //B
+            {16, 30}, // I
+            {31, 45}, // N
+            {46, 60}, // G
+            {61, 75} // O
+    };
+
     private final int[][] numeros;
 
     public BingoCard() {
@@ -15,23 +23,51 @@ public class BingoCard {
     }
 
     private void gerarNumeros() {
-        List<Integer> pool = new ArrayList<>();
-        for (int i = 1; i <= 75; i++) {
-            pool.add(i);
-        }
-        Collections.shuffle(pool);
+        for(int col = 0; col < COLUNAS; col++) {
+            // Gera uma lista de números para a coluna atual dentro do intervalo
+            List<Integer> numerosColuna = gerarNumerosColuna(FAIXA[col][0], FAIXA[col][1]);
 
-        int index = 0;
-        for (int i = 0; i < LINHAS; i++) {
-            for (int j = 0; j < COLUNAS; j++) {
-                if(i == 2 && j == 2){
-                    numeros[i][j] = 0; //Posição no meio da cartela zerada.
+            for(int row = 0; row < LINHAS; row++) {
+                if(col == 2 && row == 2) {
+                    numeros[row][col] = 0; //Posição no meio da cartela zerada.
                 } else {
-                    numeros[i][j] = pool.get(index++);
+                    numeros[row][col] = numerosColuna.get(row);
                 }
             }
         }
+
+//        List<Integer> pool = new ArrayList<>();
+//        for (int i = 1; i <= 75; i++) {
+//            pool.add(i);
+//        }
+//        Collections.shuffle(pool);
+//
+//        int index = 0;
+//        for (int i = 0; i < LINHAS; i++) {
+//            for (int j = 0; j < COLUNAS; j++) {
+//                if(i == 2 && j == 2){
+//                    numeros[i][j] = 0; //Posição no meio da cartela zerada.
+//                } else {
+//                    numeros[i][j] = pool.get(index++);
+//                }
+//            }
+//        }
     }
+
+    private List<Integer> gerarNumerosColuna(int min, int max) {
+        List<Integer> numerosCol = new ArrayList<>();
+        for(int i = min; i <= max; i++) {
+            numerosCol.add(i);
+        }
+
+        //Embaralhando números e pegando os x primeiros baseado na quantidade de linhas.
+        Collections.shuffle(numerosCol);
+        List<Integer> numerosColuna = numerosCol.subList(0, LINHAS);
+
+        //Ordenando números.
+        Collections.sort(numerosColuna);
+        return numerosColuna;
+    };
 
     public int[][] getNumeros() {
         return numeros;
